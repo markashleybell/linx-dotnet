017345a5cb1c328fd48f700b15c2ab3341b6c048
Mark Bell <me@markb.co.uk>
2020-12-26 13:53:24 +0000

    Get rid of some unnecessary dependencies, upgrade a few

--- linx-dotnet/webpack.config.js
+++ linx-dotnet/webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
             { test: /\.tsx?$/, exclude: /node_modules/, loader: 'ts-loader' },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
-            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-            { test: /\.css$/i, use: ['style-loader', 'css-loader'] }
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
         ]
     },
     externals: {
