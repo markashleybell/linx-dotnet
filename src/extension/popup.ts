31ef7cabbad92826b0eebc31b67ca64c2e77c15a
Mark Bell <me@markb.co.uk>
2021-02-07 11:13:20 +0000

    Validation in extension

--- src/extension/popup.ts
+++ src/extension/popup.ts
@@ -6,14 +6,30 @@ interface PageDetails {
     abstract: string;
 }
 
+interface ApiValidationError {
+    key: string;
+    value: string[];
+}
+
+interface ApiErrorResponse {
+    errors: ApiValidationError[];
+}
+
 const form = document.getElementById("bookmark-details") as HTMLFormElement;
 
 const status = document.getElementById("status") as HTMLSpanElement;
 
 const titleInput = document.getElementById("title") as HTMLInputElement;
 const urlInput = document.getElementById("url") as HTMLInputElement;
-const abstractInput = document.getElementById("abstract") as HTMLInputElement;
 const tagsInput = document.getElementById("tags") as HTMLInputElement;
+const abstractInput = document.getElementById("abstract") as HTMLInputElement;
+
+const inputs: Record<string, HTMLInputElement> = {
+    'Title': titleInput,
+    'Url': urlInput,
+    'Tags': tagsInput,
+    'Abstract': abstractInput
+}
 
 // This callback function is called when the content script
 // has been injected and returned its results
@@ -38,6 +54,10 @@ window.addEventListener("load", () => {
         form.addEventListener("submit", async (event: Event) => {
             event.preventDefault();
         
+            for (const k in inputs) {
+                inputs[k].classList.remove('is-invalid');
+            }
+
             showInfoStatus(status, "Saving...");
         
             try {
@@ -50,9 +70,17 @@ window.addEventListener("load", () => {
                 });
         
                 showSuccessStatus(status, "Saved", 1000);
+                
                 // window.setTimeout(window.close, 1000);
             } catch (e) {
-                // alert(JSON.stringify(e));
+                const errors = (e as ApiErrorResponse).errors;
+
+                for (const error of errors) {
+                    const input = inputs[error.key];
+                    (input.nextElementSibling as HTMLDivElement).innerText = error.value.join('');
+                    input.classList.add('is-invalid');
+                }
+
                 showErrorStatus(status, "Error");
             }
         });
