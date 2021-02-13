a7958a37e50876d379b5b54f91870c3eb0b979f1
Mark Bell <me@markb.co.uk>
2021-02-13 06:58:26 +0000

    More WIP

---
+++ src/app/Support/TagLinksTagHelper.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using Linx.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Linx.Support
+{
+    public class TagLinksTagHelper : TagHelper
+    {
+        private readonly IHtmlGenerator _htmlGenerator;
+
+        public TagLinksTagHelper(IHtmlGenerator htmlGenerator) =>
+            _htmlGenerator = htmlGenerator;
+
+        public IEnumerable<Tag> Tags { get; set; }
+
+        public string ContainerClasses { get; set; }
+            = "tag-links";
+
+        public string TagClasses { get; set; }
+            = "badge bg-primary";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.Attributes.Add("class", ContainerClasses);
+
+            foreach (var link in Tags.Select(t => TagSearchActionLink(t.Label)))
+            {
+                output.Content.AppendHtml(link);
+            }
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+
+        private TagBuilder TagSearchActionLink(string label) =>
+            _htmlGenerator.GenerateActionLink(
+                viewContext: ViewContext,
+                linkText: label,
+                actionName: "Index",
+                controllerName: "Search",
+                protocol: null,
+                hostname: null,
+                fragment: null,
+                routeValues: new { query = $"[{label}]" },
+                htmlAttributes: new { @class = TagClasses }
+            );
+    }
+}
