006848f4f0159f99d34dba4a025d8c53871f9754
Mark Bell <me@markb.co.uk>
2021-02-14 07:48:05 +0000

    Extension now uses new icon

--- src/extension/webpack.config.js
+++ src/extension/webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'icon.png', to: '.' },
+                { from: 'favicon-16x16.png', to: '.' },
+                { from: 'favicon-32x32.png', to: '.' },
+                { from: 'icon-192x192.png', to: '.' },
+                { from: 'icon-512x512.png', to: '.' },
+
                 { from: 'manifest.json', to: '.' },
                 { from: 'settings.html', to: '.' },
                 { from: 'popup.html', to: '.' },
