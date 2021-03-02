d13d7764c8168eac0caa8ff4fbf26667ac8b5b93
Mark Bell <me@markb.co.uk>
2021-03-02 07:09:22 +0000

    Pagination is still surprisingly hard... closes #1

--- src/app/Models/PaginationDetails.cs
+++ src/app/Models/PaginationDetails.cs
@@ -15,6 +15,32 @@ namespace Linx.Models
 
         public int Page { get; set; }
 
+        public int DirectPageLinksEitherSideOfCurrent { get; set; } = 5;
+
+        public IEnumerable<int> DirectPageLinksBefore
+        {
+            get
+            {
+                var start = Page - DirectPageLinksEitherSideOfCurrent;
+
+                return start < 1
+                    ? Enumerable.Range(1, DirectPageLinksEitherSideOfCurrent + start - 1)
+                    : Enumerable.Range(start, DirectPageLinksEitherSideOfCurrent);
+            }
+        }
+
+        public IEnumerable<int> DirectPageLinksAfter
+        {
+            get
+            {
+                var end = Page + DirectPageLinksEitherSideOfCurrent;
+
+                return end > Pages
+                    ? Enumerable.Range(Page + 1, Pages - Page)
+                    : Enumerable.Range(Page + 1, DirectPageLinksEitherSideOfCurrent);
+            }
+        }
+
         public SortColumn Sort { get; set; }
 
         public SortDirection SortDirection { get; set; }
